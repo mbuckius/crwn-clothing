f115c2a6719d2f931ce886f664cfab2fdc606dea
Mayuri Buckius <mbuckius@uci.edu>
2022-08-01 10:40:20 -0500

    memoize category.selector

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,11 +1,22 @@
-export const selectCategoriesMap = (state) => {
-  console.log('selector fired');
+import { createSelector } from 'reselect';
 
-  return state.categories.categories.reduce((acc, category) => {
-      const { title, items } = category;  
-      acc[title.toLowerCase()] = items;
-      return acc;
-  }, {});
-};
+//create memoized selector
+//return previously calculated value instead of rerunning code 
+
+const selectCategoryReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => categoriesSlice.categories
+);
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) => categories.reduce((acc, category) => {
+    const { title, items } = category;  
+    acc[title.toLowerCase()] = items;
+    return acc;
+  }, {})
+);
  
   
