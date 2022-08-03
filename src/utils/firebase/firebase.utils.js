134d06d77e78edad7cba58ea78414979927eef44
Mayuri Buckius <mbuckius@uci.edu>
2022-08-03 12:38:07 -0500

    add firebase config

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -21,12 +21,12 @@ import {
 } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-  projectId: 'crwn-clothing-db-98d4d',
-  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-  messagingSenderId: '626766232035',
-  appId: '1:626766232035:web:506621582dab103a4d08d6',
+  apiKey: "AIzaSyCI9zC-oA2x45bgDVb4tcROvuSppZBQeu4",
+  authDomain: "crwn-clothing-db-6441c.firebaseapp.com",
+  projectId: "crwn-clothing-db-6441c",
+  storageBucket: "crwn-clothing-db-6441c.appspot.com",
+  messagingSenderId: "342956039311",
+  appId: "1:342956039311:web:5efbae42903542e746e4fa"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
