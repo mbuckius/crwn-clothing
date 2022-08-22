3e23fbc3289171ec805cc7c990210b649b68049b
Mayuri Buckius <mbuckius@uci.edu>
2022-08-22 12:15:21 -0500

    update

--- src/store/store.js
+++ src/store/store.js
@@ -1,30 +1,32 @@
-import { legacy_createStore as createStore } from "redux"
-import { compose, applyMiddleware } from 'redux';
+import { compose, createStore, applyMiddleware } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
-import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+
+import { rootSaga } from './root-saga';
 
 import { rootReducer } from './root-reducer';
 
 const persistConfig = {
   key: 'root',
   storage,
-  blacklist: ['user']
+  whitelist: ['cart'],
 };
+
+const sagaMiddleware = createSagaMiddleware();
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-//only want to render logger in development
-//.filter(Boolean) filters out anything that is false
-//instead it will evaluate to an empty object if we are not in development
-const middleWares = [process.env.NODE_ENV === 'development' && logger, thunk].filter(Boolean);
+const middleWares = [
+  process.env.NODE_ENV !== 'production' && logger,
+  sagaMiddleware,
+].filter(Boolean);
 
-//allow Redux Devtools
-const composeEnhancer = 
-  (process.env.NODE_ENV !== 'production' && 
-    window && 
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || 
+const composeEnhancer =
+  (process.env.NODE_ENV !== 'production' &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
   compose;
 
 const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
@@ -32,6 +34,9 @@ const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 export const store = createStore(
   persistedReducer,
   undefined,
-  composedEnhancers);
+  composedEnhancers
+);
+
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
