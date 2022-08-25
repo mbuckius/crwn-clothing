ec8fcad38bff92ad68763c9457011abacaf9495a
Mayuri Buckius <mbuckius@uci.edu>
2022-08-24 20:33:22 -0500

    Add margin

--- src/components/payment-form/payment-form.styles.jsx
+++ src/components/payment-form/payment-form.styles.jsx
@@ -7,6 +7,7 @@ export const PaymentFormContainer = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
+    margin-bottom: 100px;
 `;
 
 export const FormContainer = styled.form`
