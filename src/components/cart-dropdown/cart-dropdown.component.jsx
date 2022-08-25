a61d8965b473a08e4063af5d9b029e09a310dd49
Mayuri Buckius <mbuckius@uci.edu>
2022-08-24 20:30:41 -0500

    hide cart dropdown when moving to check out page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,9 +1,14 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
-import { selectCartItems } from '../../store/cart/cart.selector';
+
+import {
+  selectCartItems,
+  selectIsCartOpen,
+} from '../../store/cart/cart.selector';
+import { setIsCartOpen } from '../../store/cart/cart.action.js';
 
 import {
   CartDropdownContainer,
@@ -12,10 +17,16 @@ import {
 } from './cart-dropdown.styles';
 
 const CartDropdown = () => {
+  const dispatch = useDispatch();
+
   const cartItems = useSelector(selectCartItems);
+  const isCartOpen = useSelector(selectIsCartOpen);
+
   const navigate = useNavigate();
 
   const goToCheckoutHandler = () => {
+    //hide cart dropdown when moving to checkout
+    dispatch(setIsCartOpen(!isCartOpen));
     navigate('/checkout');
   };
 
