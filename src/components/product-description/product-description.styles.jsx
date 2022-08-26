41c6a54651fe3cd557479511ae56f19c94f973e2
Mayuri Buckius <mbuckius@uci.edu>
2022-08-26 10:44:28 -0500

    Added styles

--- src/components/product-description/product-description.styles.jsx
+++ src/components/product-description/product-description.styles.jsx
@@ -1,8 +1,30 @@
 import styled from "styled-components";
 
 export const ProductDescriptionContainer = styled.div`
-    height: 400px;
+    width: 100%;
+    height: 500px;
     margin: 20px 0px;
     font-size: 18px;
     overflow: scroll;
+`;
+
+export const Info = styled.div`
+    border-style: solid none;
+    border-color: gainsboro;
+    cursor: pointer;
+
+    :last-child {
+        border-top-style: none;
+    }
+`;
+
+export const Subheading = styled.div`
+    text-transform: uppercase;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    span {
+        font-size: 30px;
+    }
 `;
