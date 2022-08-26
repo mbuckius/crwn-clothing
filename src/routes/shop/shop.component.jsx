c7808259fb2445e1611f0fd1bfc374b5a83b7019
Mayuri Buckius <mbuckius@uci.edu>
2022-08-25 22:16:43 -0500

    Add routes for products

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -4,6 +4,7 @@ import { useDispatch } from 'react-redux';
 
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
+import CategoryRoute from '../category-route/category-route.component';
 import { fetchCategoriesStart } from '../../store/categories/category.action';
 
 const Shop = () => {
@@ -16,7 +17,7 @@ const Shop = () => {
   return (
     <Routes>
       <Route index element={<CategoriesPreview />} />
-      <Route path=':category' element={<Category />} />
+      <Route path=':category/*' element={<CategoryRoute />} />
     </Routes>
   );
 };
