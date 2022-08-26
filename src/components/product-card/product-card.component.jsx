c3c88b43174f2d47289dbffd14fd76ffd893ad51
Mayuri Buckius <mbuckius@uci.edu>
2022-08-25 22:13:19 -0500

    add link to product page

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,28 +1,32 @@
 import { useDispatch, useSelector } from 'react-redux';
-
+import { useNavigate } from 'react-router-dom';
 import { selectCartItems } from '../../store/cart/cart.selector';
 import { addItemToCart } from '../../store/cart/cart.action';
 
 import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
 
 import {
-  ProductCartContainer,
+  ProductCardContainer,
   Footer,
   Name,
   Price,
 } from './product-card.styles';
 
 const ProductCard = ({ product }) => {
-  const { name, price, imageUrl } = product;
+  // Destructure product object
+  const { id, name, price, imageUrl } = product;
   const dispatch = useDispatch();
+
+  // Get cartItems using selector
   const cartItems = useSelector(selectCartItems);
 
+  // onClick function when user cliakcs "Add to cart" button
   const addProductToCart = () => dispatch(addItemToCart(cartItems, product));
 
   return (
-    <ProductCartContainer>
+    <ProductCardContainer>
       <img src={imageUrl} alt={`${name}`} />
-      <Footer>
+      <Footer to={`${id}`}>
         <Name>{name}</Name>
         <Price>{price}</Price>
       </Footer>
@@ -32,7 +36,7 @@ const ProductCard = ({ product }) => {
       >
         Add to cart
       </Button>
-    </ProductCartContainer>
+    </ProductCardContainer>
   );
 };
 
