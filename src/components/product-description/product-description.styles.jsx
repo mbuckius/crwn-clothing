a3553feebcd2d221fa417cdc184f6d0895e20a38
Mayuri Buckius <mbuckius@uci.edu>
2022-08-25 23:25:01 -0500

    Add scroll

--- src/components/product-description/product-description.styles.jsx
+++ src/components/product-description/product-description.styles.jsx
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 
 export const ProductDescriptionContainer = styled.div`
+    height: 400px;
     margin: 20px 0px;
     font-size: 18px;
+    overflow: scroll;
 `;
