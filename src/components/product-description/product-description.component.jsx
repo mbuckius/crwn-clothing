9da98551b16582d5a44731a00a6122773769ea8e
Mayuri Buckius <mbuckius@uci.edu>
2022-08-26 10:44:18 -0500

    Add functionality

--- src/components/product-description/product-description.component.jsx
+++ src/components/product-description/product-description.component.jsx
@@ -1,26 +1,44 @@
 import parse from 'html-react-parser';
+import { useState } from 'react';
 
-import { ProductDescriptionContainer } from './product-description.styles';
+import { 
+    ProductDescriptionContainer, 
+    Info, 
+    Subheading } from './product-description.styles';
 
-const ProductDescription = ({ description, material }) => {
+const ProductDescription = ({ description, material, price }) => {
+    
+    //  Create bools showDetails and showMaterials which indicate if text should render
+    const [showDetails, setShowDetails] = useState(false);
+    const [showMaterials, setShowMaterials] = useState(false);
 
-    // var htmlObject = document.createElement('div');
-    // htmlObject.innerHTML = description;
-
-    // return (
-    //     <h2>{description}</h2>
-    // );
+    // Functions to toggle showDetails and showMaterials, called when user clicks on subheading
+    const toggleShowDetails = () => (setShowDetails(!showDetails));
+    const toggleShowMaterials = () => (setShowMaterials(!showMaterials));
 
     return (
         <ProductDescriptionContainer>
-            {description? parse(description) : "description"}
-            <br />
-            <br />
-            <p>Material and Care:</p>
-            {material? parse(material) : "material"}
-        </ProductDescriptionContainer>
-    )
+            <h3>{`$${price}`}</h3>
 
+            <Info onClick={toggleShowDetails}>
+                <Subheading>
+                    <h4>Details:</h4>
+                    { showDetails? (<span>&#8722;</span>) : (<span>&#43;</span>) } 
+                </Subheading>
+                
+                { showDetails && (description? parse(description) : "description")}
+            </Info>
+
+            <Info onClick={toggleShowMaterials}>
+                <Subheading>
+                    <h4>Material and Care:</h4>
+                    { showMaterials? (<span>&#8722;</span>) : (<span>&#43;</span>) } 
+                </Subheading>   
+
+                { showMaterials && (material? parse(material) : "material")}
+            </Info>
+        </ProductDescriptionContainer>
+    );
 };
 
 export default ProductDescription;
