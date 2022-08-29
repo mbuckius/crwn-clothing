c21db593807b88c41bcc5beb861a82ca354c3403
Mayuri Buckius <mbuckius@uci.edu>
2022-08-28 19:12:02 -0500

    Add styles

---
+++ src/routes/search/search.styles.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const SearchContainer = styled.div`
+    margin: 50px auto; 
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 800px;
+    height: 700px;
+    justify-content: 
+    column-gap: 20px;
+    margin: 30px auto;  
+`;
+
+export const Title = styled.h2`
+    text-transform: uppercase;
+    font-size: 40px;
+    margin-bottom: 25px;
+    text-align: center;
+`;
