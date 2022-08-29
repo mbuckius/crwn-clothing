f5d9a488843a60d3b66d9d3df4e809bc72661312
Mayuri Buckius <mbuckius@uci.edu>
2022-08-28 19:10:21 -0500

    Add styles

---
+++ src/components/search-box/search-box.styles.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const SearchBoxContainer = styled.input`
+    -webkit-appearance: none;
+    border: solid;
+    border-color: grey;
+    outline: none;
+    padding: 10px;
+    width: 300px;
+    line-height: 30px;
+    margin-bottom: 30px;
+`;
