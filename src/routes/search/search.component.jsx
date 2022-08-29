b1dcf65afc32683d50bc25bb85f756881b8ec69f
Mayuri Buckius <mbuckius@uci.edu>
2022-08-28 20:39:36 -0500

    Clean code

--- src/routes/search/search.component.jsx
+++ src/routes/search/search.component.jsx
@@ -3,7 +3,6 @@ import { useSelector, useDispatch } from 'react-redux';
 
 import Spinner from '../../components/spinner/spinner.component';
 import SearchBox from "../../components/search-box/search-box.component";
-import SearchResults from '../../components/search-results/search-results.component';
 
 import { fetchCategoriesStart } from '../../store/categories/category.action';
 import {
@@ -12,93 +11,39 @@ import {
 } from '../../store/categories/category.selector';
 
 import { SearchContainer, Title } from './search.styles';
+import { useNavigate } from 'react-router-dom';
 
 const Search = () => {
-    const dispatch = useDispatch();
-
-    // get categories from firebase, run only at first
-    useEffect(() => {
-        dispatch(fetchCategoriesStart());
-    }, []);
-
-    // Get categoriesMap and isLoading state using selectors
-    const categoriesMap = useSelector(selectCategoriesMap);
-    const isLoading = useSelector(selectCategoriesIsLoading);
+    const navigate = useNavigate();
 
     // Create searchField, showResults, and products array
     const [searchField, setSearchField] = useState('');
-    const [products, setProducts] = useState([]);
-    const [filteredProductsMap, setFilteredProductsMap] = useState(categoriesMap);
-    const [resultCount, setResultCount] = useState(0);
     
     // called when input in SearchBox changes (user types/deletes letters)
     const onSearchChange = (event) => {
-        // const searchFieldString = event.target.value.toLocaleLowerCase();
-        // setSearchField(searchFieldString); 
+        const searchFieldString = event.target.value.toLocaleLowerCase();
+        setSearchField(searchFieldString); 
     };
 
     // Runs whenever user enters a key
     const onEnter = (event) => {
-
-        // Want search to run when user presses Enter
+        // Want to go to search results page when user presses Enter
         if (event.key ==='Enter') {
-            console.log("user hit enter");
-
-            //set searchField to what user typed
-            const searchFieldString = event.target.value.toLocaleLowerCase();
-            setSearchField(searchFieldString); 
+            navigate(`/search/${searchField}`);
         }
     };
 
-    // When searchField changes, filter categoriesMap with includes
-    useEffect(() => {
-        // reset resultCount 
-        setResultCount(0);
-
-        // create a tempMap which has same contents as categoriesMap
-        var tempMap = JSON.parse(JSON.stringify(categoriesMap));
-        
-        // for each category in categoriesMap 
-        Object.keys(categoriesMap).map((category) => {
-            
-            // Create temp array which will hold filtered products in current category
-            const temp = (categoriesMap[category]).filter((product) => {
-                return product.name.toLocaleLowerCase().includes(searchField);
-            });
-            
-            // Update tempMap and resultCount
-            tempMap[category] = temp;
-            setResultCount(prevCount => (prevCount + temp.length));
-        });
-
-        // Update filteredProductsMap
-        setFilteredProductsMap(tempMap);
-    }, [searchField, categoriesMap]); 
-
     return (
-        <Fragment>
-            {isLoading ? (
-                <Spinner />
-            ) : (
-                <div>
-                    {products && (
-                        <SearchContainer>
-                            <Title>Search</Title>
-                            <SearchBox 
-                                placeholder='&#128269;  Search for Products' 
-                                onChangeHandler ={ onSearchChange }
-                                onKeyPress = { onEnter } 
-                            />
-
-                            {searchField &&
-                                <SearchResults filteredProductsMap={ filteredProductsMap } resultCount={ resultCount }/>
-                            }
-                        </SearchContainer>
-                    )}
-                </div>
-            )}
-        </Fragment>
-        
+        <div>
+            <SearchContainer>
+                <Title>Search</Title>
+                <SearchBox 
+                    placeholder='&#128269;  Search for Products' 
+                    onChangeHandler ={ onSearchChange }
+                    onKeyPress = { onEnter } 
+                />
+            </SearchContainer>
+        </div>
     );
 };
 
