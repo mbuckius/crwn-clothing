e4074a967c6c18629b6ab889fe006555a83bc31c
Mayuri Buckius <mbuckius@uci.edu>
2022-08-28 19:11:00 -0500

    Add styles

--- src/components/search-results/search-results.styles.jsx
+++ src/components/search-results/search-results.styles.jsx
@@ -1,10 +1,14 @@
 import styled from 'styled-components';
 
 export const ResultsContainer = styled.div`
-  margin: 50px auto;
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  column-gap: 20px;
-  row-gap: 50px;
+    
+`;
+
+export const ProductCards = styled.div`
+    margin: 50px auto;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    column-gap: 20px;
+    row-gap: 50px;
 `;
 
